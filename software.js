be3c33c9ca82297acb6ee5ccaae3052deac960f3
Theo1461 <july01keartonhill@gmail.com>
2025-01-19 22:37:37 -0500

    Add files via upload

---
+++ software.js
@@ -0,0 +1,37 @@
+const softwareVersion = "211";
+//Base//
+let savedPoints = 0
+let rolledPoints = 0
+let sidePoints = 0
+let rollCount = 0
+let fireUp = false
+let waterUp = false
+let breezeUp = false
+//2.10//
+
+//2.10.0.01//
+let rain = false
+let mudUp = false
+let sunnySeeds = false
+let sunnyOne = false
+let sunnyTwo = false
+let sunnyThree = false
+//2.10.0.02//
+let obsidianRolled = false
+let earthRolled = false
+//2.10.0.03//
+let settingsOpen = false
+//2.10.0.4//
+let background = 0
+let ach1000 = false
+let multiplier = 1
+let bgRounds = 0
+//2.10.0.5//
+let cactusUp = false
+let goldUp = false
+//2.10.0.6//
+let tube = 0
+let unlocked1 = false
+//2.10.0.7//
+let seeds = 0
+let seedUp = false
